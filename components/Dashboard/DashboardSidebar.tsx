c527be4bf98fc54066041e228fb016c3aaed8ad5
k8pai <thek8pai@gmail.com>
2023-10-06 23:52:33 +0530

    Group features to be added

--- components/Dashboard/DashboardSidebar.tsx
+++ components/Dashboard/DashboardSidebar.tsx
@@ -2,87 +2,106 @@ import clsx from "clsx";
 import { useRouter } from "next/router";
 import { ComponentProps, useMemo } from "react";
 import {
-  DASHBOARD_DRAFTS_URL,
-  DASHBOARD_GROUP_URL,
-  DASHBOARD_URL,
+    DASHBOARD_DRAFTS_URL,
+    DASHBOARD_GROUP_URL,
+    DASHBOARD_URL,
 } from "../../constants";
-import { FileIcon, FolderIcon } from "../../icons";
-import { LinkButton } from "../../primitives/Button";
+import { FileIcon, FolderIcon, PlusIcon } from "../../icons";
+import { Button, LinkButton } from "../../primitives/Button";
 import { Group } from "../../types";
 import { normalizeTrailingSlash } from "../../utils";
 import styles from "./DashboardSidebar.module.css";
 
 interface Props extends ComponentProps<"div"> {
-  groups: Group[];
+    groups: Group[];
 }
 
 interface SidebarLinkProps
-  extends Omit<ComponentProps<typeof LinkButton>, "href"> {
-  href: string;
+    extends Omit<ComponentProps<typeof LinkButton>, "href"> {
+    href: string;
 }
 
 function SidebarLink({
-  href,
-  children,
-  className,
-  ...props
+    href,
+    children,
+    className,
+    ...props
 }: SidebarLinkProps) {
-  const router = useRouter();
-  const isActive = useMemo(
-    () =>
-      normalizeTrailingSlash(router.asPath) === normalizeTrailingSlash(href),
-    [router, href]
-  );
+    const router = useRouter();
+    const isActive = useMemo(
+        () =>
+            normalizeTrailingSlash(router.asPath) ===
+            normalizeTrailingSlash(href),
+        [router, href]
+    );
 
-  return (
-    <LinkButton
-      className={clsx(className, styles.sidebarLink)}
-      data-active={isActive || undefined}
-      href={href}
-      variant="subtle"
-      {...props}
-    >
-      {children}
-    </LinkButton>
-  );
+    return (
+        <LinkButton
+            className={clsx(className, styles.sidebarLink)}
+            data-active={isActive || undefined}
+            href={href}
+            variant="subtle"
+            {...props}
+        >
+            {children}
+        </LinkButton>
+    );
 }
 
 export function DashboardSidebar({ className, groups, ...props }: Props) {
-  return (
-    <div className={clsx(className, styles.sidebar)} {...props}>
-      <nav className={styles.navigation}>
-        <div className={styles.category}>
-          <ul className={styles.list}>
-            <li>
-              <SidebarLink href={DASHBOARD_URL} icon={<FileIcon />}>
-                All
-              </SidebarLink>
-            </li>
-            <li>
-              <SidebarLink href={DASHBOARD_DRAFTS_URL} icon={<FileIcon />}>
-                Drafts
-              </SidebarLink>
-            </li>
-          </ul>
+    return (
+        <div className={clsx(className, styles.sidebar)} {...props}>
+            <nav className={styles.navigation}>
+                <div className={styles.category}>
+                    <ul className={styles.list}>
+                        <li>
+                            <SidebarLink
+                                href={DASHBOARD_URL}
+                                icon={<FileIcon />}
+                            >
+                                All
+                            </SidebarLink>
+                        </li>
+                        <li>
+                            <SidebarLink
+                                href={DASHBOARD_DRAFTS_URL}
+                                icon={<FileIcon />}
+                            >
+                                Drafts
+                            </SidebarLink>
+                        </li>
+                    </ul>
+                </div>
+                <div className={styles.category}>
+                    <span className={styles.categoryTitle}>Groups</span>
+                    <ul className={styles.list}>
+                        {groups.map((group) => {
+                            return (
+                                <li key={group.id}>
+                                    <SidebarLink
+                                        href={DASHBOARD_GROUP_URL(group.id)}
+                                        icon={<FolderIcon />}
+                                    >
+                                        {group.name}
+                                    </SidebarLink>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+
+                {/* adding new groups functionality to be added. */}
+                <div className={styles.category}>
+                    <Button
+                        variant="subtle"
+                        icon={<PlusIcon />}
+                        onClick={() => console.log("created a new group...")}
+                        className={styles.groupButton}
+                    >
+                        Add Group
+                    </Button>
+                </div>
+            </nav>
         </div>
-        <div className={styles.category}>
-          <span className={styles.categoryTitle}>Groups</span>
-          <ul className={styles.list}>
-            {groups.map((group) => {
-              return (
-                <li key={group.id}>
-                  <SidebarLink
-                    href={DASHBOARD_GROUP_URL(group.id)}
-                    icon={<FolderIcon />}
-                  >
-                    {group.name}
-                  </SidebarLink>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      </nav>
-    </div>
-  );
+    );
 }
